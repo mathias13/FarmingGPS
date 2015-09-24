e5d7f16b4ac4f7aa4a8ac1ad627466a91972d887
mathias13 <mathias@nolgarden.net>
2015-09-24 23:11:59 +0200

    Added settings interface

---
+++ FarmingGPS/Settings/ISetting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FarmingGPS.Settings
+{
+    public interface ISetting
+    {
+        string Name
+        {
+            get;
+        }
+
+        public Type ValueType
+        {
+            get;
+        }
+
+        public object Value
+        {
+            get;
+        }
+    }
+}
---
+++ FarmingGPS/Settings/ISettingsCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FarmingGPS.Settings
+{
+    public interface ISettingsCollection : ICollection<ISetting>
+    {
+        string Name
+        {
+            get;
+        }
+    }
+}
