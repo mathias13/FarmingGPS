bad470a13b40a055dcb92ab983a7cb9ae4b25031
Mathias Karlsson <mathias@nolgarden.net>
2024-04-19 07:57:51 +0200

    Fixed bug

--- FarmingGPS/Visualization/EquipmentLevelBar.xaml.cs
+++ FarmingGPS/Visualization/EquipmentLevelBar.xaml.cs
@@ -15,7 +15,6 @@ namespace FarmingGPS.Visualization
         public EquipmentLevelBar()
         {
             InitializeComponent();
-
         }
         
         #region Public Methods
@@ -39,7 +38,7 @@ namespace FarmingGPS.Visualization
 
                 SetValue(FillLevelString, fillLevel.ToString("0 %"));
                 SetValue(WidthPercentage, _backGround.ActualWidth * fillLevel);
-                SetValue(FillColor, fillLevel > 10.0 ? Colors.LightGreen : Colors.Red);
+                SetValue(FillColor, fillLevel > 10.0 ? new SolidColorBrush(Colors.LightGreen) : new SolidColorBrush(Colors.Red));
             }
             else
                 Dispatcher.BeginInvoke(new Action<object, EventArgs>(EquipmentStat_StatUpdated), System.Windows.Threading.DispatcherPriority.Normal, sender, e);
